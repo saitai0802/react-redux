01f68731dc6fbeac82ce278fe14b132d515350a6
saitai0802 <jazzsaitai@gmail.com>
2017-08-28 15:42:53 +0800

    Added some comments
    Added some comments  to clarify those codings more clearly

--- ReduxCasts/book_list/src/reducers/index.js
+++ ReduxCasts/book_list/src/reducers/index.js
@@ -4,8 +4,8 @@ import ActiveBook from "./reducer_active_book";
 
 // Any key to the object that we provide to combine reducers ends up a key on our global state.
 const rootReducer = combineReducers({
-  books: BooksReducer,
-  activeBook: ActiveBook
+  books: BooksReducer,  // Only static valuable return.
+  activeBook: ActiveBook // Will get the dynamic valuable of different action type.
 });
 
 export default rootReducer;
